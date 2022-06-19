a14bc7a411f59ba238e3235ff24c8e3bdd9a20ef
coloraddict <ajayvjaiswar@gmail.com>
2022-06-19 11:49:34 +0530

    changes measurement from standard to metric to get in celcius

--- src/app/shared/services/weather-forecast.service.ts
+++ src/app/shared/services/weather-forecast.service.ts
@@ -24,10 +24,10 @@ export class WeatherForecastService {
 
   getWeather(city){
     // console.log(city);
-    return this.http.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a50a790c1717bcbc09c88e191997fe3f");
+    return this.http.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a50a790c1717bcbc09c88e191997fe3f&units=metric");
   }
 
   getForecast(p_lat, p_lon){
-    return this.http.get("https://api.openweathermap.org/data/2.5/forecast?lat=" + p_lat + "&lon=" + p_lon + "&appid=a50a790c1717bcbc09c88e191997fe3f");
+    return this.http.get("https://api.openweathermap.org/data/2.5/forecast?lat=" + p_lat + "&lon=" + p_lon + "&appid=a50a790c1717bcbc09c88e191997fe3f&units=metric");
   }
 }
